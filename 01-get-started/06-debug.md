8636718ca970c089970bedd14c51494f55fe6378
lisa-lumos <zjcty307@gmail.com>
2023-04-27 11:23:20 -0600

    add debugger info

--- 01-get-started/06-debug.md
+++ 01-get-started/06-debug.md
@@ -1,1 +1,16 @@
 # 6. debug C# console apps
+Mastering the use of a code debugger is an important skill. 
+
+- Testing can be categorized into functional and nonfunctional testing, and developers are expected to perform some level of testing.
+- Code debugging is the process of isolating issues and identifying ways to fix them, and it's a developer responsibility.
+- Exception handling is the process of managing errors that occur during runtime, and developers are responsible for handling exceptions by using "try" and "catch" statements in their code.
+
+A debugger is a software tool used to observe and control the execution flow of your program with an analytical approach. Its primary benefit is that you can watch your program running. You can follow program execution one line of code at a time. This approach minimizes the chance of guessing wrong.
+
+Code to debug: 
+```cs
+string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+int studentCount = students.Length;
+Console.WriteLine("The final name is: " + students[studentCount]);
+```
+
--- 01-get-started/code/app1/Program.cs
+++ 01-get-started/code/app1/Program.cs
@@ -81,23 +81,104 @@
 
 // int[] data = new int[3];
 
+// SayHello();
+
+// void SayHello() {
+//     Console.WriteLine("Hello World!");
+// }
+
+// int[] a = {1,2,3,4,5};
+
+// Console.WriteLine("Contents of Array:");
+// PrintArray();
+// // Contents of Array:
+// // 1 2 3 4 5 
+
+// void PrintArray() {
+//     foreach (int x in a) {
+//         Console.Write($"{x} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
+// string[] address;
+// bool validLength = false;
+// bool validZeroes = false;
+// bool validRange = false;
+
+// foreach (string ip in ipv4Input) 
+// {
+//     address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+//     ValidateLength(); 
+//     ValidateZeroes(); 
+//     ValidateRange();
+
+//     if (validLength && validZeroes && validRange) 
+//     {
+//         Console.WriteLine($"{ip} is a valid IPv4 address");
+//     } 
+//     else 
+//     {
+//         Console.WriteLine($"{ip} is an invalid IPv4 address");
+//     }
+// }
+
+// void ValidateLength() 
+// {
+//     validLength = address.Length == 4;
+// }
+
+// void ValidateZeroes() 
+// {
+//     foreach (string number in address) 
+//     {
+//         if (number.Length > 1 && number.StartsWith("0")) 
+//         {
+//             validZeroes = false;
+//             return;
+//         }
+//     }
+
+//     validZeroes = true;
+// }
+
+// void ValidateRange() 
+// {
+//     foreach (string number in address) 
+//     {
+//         if (int.Parse(number) > 255) 
+//         {
+//             validRange = false;
+//             return;
+//         }
+//     }
+//     validRange = true;
+// }
 
 
 
+// string[] text = {"You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"};
+// string[] good = {"look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"};
+// string[] bad = {"fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."};
+// string[] neutral = {"appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."};
 
+// Random random = new Random();
+// int luck = random.Next(100);
+// TellFortune();
 
+// void TellFortune() 
+// {
+//     Console.WriteLine("A fortune teller whispers the following words:");
+//     // string[] fortune = good; // assign reference name to another reference, they all point to the same string array
+//     string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral)); 
+//     for (int i = 0; i < 4; i++) 
+//     {
+//         Console.Write($"{text[i]} {fortune[i]} ");
+//     }
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+// int studentCount = students.Length;
+// Console.WriteLine("The final name is: " + students[studentCount]);
